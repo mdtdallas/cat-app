7e3a6390f19842fff7a5726ccca97673100994ec
Dallas Little <mdtdallas@hotmail.com>
2022-04-09 21:05:12 +1000

    first commit

---
+++ src/components/CatView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+
+const CatView = () => {
+  return (
+    <Card>
+
+    </Card>
+  )
+}
+
+export default CatView
---
+++ src/forms/NewCatForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCatForm = () => {
+  return (
+    <div>NewCatForm</div>
+  )
+}
+
+export default NewCatForm
